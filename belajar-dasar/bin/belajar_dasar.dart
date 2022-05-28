299a236fc649b212cd5d8f50a47339b86519d83e
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-05-28 17:44:43 +0700

    Hai

---
+++ belajar-dasar/bin/belajar_dasar.dart
@@ -0,0 +1,7 @@
+import 'package:belajar_dasar/belajar_dasar.dart' as belajar_dasar;
+
+void main(List<String> arguments) {
+  var kucing = false;
+  
+  print('Hello world: ${belajar_dasar.calculate()}! $kucing');
+}
---
+++ belajar-dasar/lib/belajar_dasar.dart
@@ -0,0 +1,15 @@
+/// Fungsi [calculate] adalah fugsi yang menghitung 6 kali 7
+int calculate() {
+  return 6 * 7;
+}
+
+void halo() {
+  int a = 10;
+  // a = 2.4; - Gabole karena koma
+
+  double b;
+  b = 2.4;
+
+  num x = 10;
+  x = 2.2;
+}
