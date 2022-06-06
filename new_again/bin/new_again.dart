ad7b780fab2c51b15d92f55d999c141bc5e72baf
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-06-06 21:33:48 +0700

    new

---
+++ new_again/bin/new_again.dart
@@ -0,0 +1,17 @@
+/* 
+dart create nama
+dart run
+*/
+
+// komentar
+/* komentar */
+
+/// komentar [main] ada fungsi [tambah]
+void main(List<String> arguments) {
+  int angka = 20;
+  print('Hello world ${angka - 1}');
+}
+
+int tambah() {
+  return 1;
+}
